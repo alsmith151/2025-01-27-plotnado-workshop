8775fb84e2515c5f7c2e5bdb7cd4b3cbbd45bc43
alsmith <asmith151@outlook.com>
2025-01-27 16:19:37 +0000

    Add scripts for starting Jupyter server with SSH tunneling and RNA-seq track overlay

---
+++ start_jupyter_server_conda.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter
+#SBATCH --output=jupyter.out
+#SBATCH --error=jupyter.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --time=8:00:00
+#SBATCH --partition=short
+
+
+#### get/set tunneling info
+XDG_RUNTIME_DIR=""
+ipnport=$(shuf -i8000-9999 -n1)
+host=$(hostname)
+
+#### print tunneling instructions to jupyter-log-{jobid}.txt
+echo -e "
+    Copy/Paste this in your local terminal on VSCode to ssh tunnel with the server on the worker node.
+    -----------------------------------------------------------------
+    ssh -N -L $ipnport:localhost:$ipnport $USER@$host
+    -----------------------------------------------------------------
+
+    Look for the http://localhost:xxxx/?token=... in the output below for the link to the jupyter server.
+    "
+
+
+jupyter lab --no-browser --port=$ipnport
+
+
+
+
